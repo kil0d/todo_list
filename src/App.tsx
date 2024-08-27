f2ebbab27e15777cf11659e4c1d3c31ed35eec00
Kirill <gogoleffc@yandex.ru>
2024-08-27 16:29:36 +0300

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,55 +2,64 @@ import {ChangeEventHandler, MouseEventHandler, useState} from 'react'
 import './App.css'
 import {Todo} from "./Todo/Todo.tsx";
 import {TodoHeadIcon} from "./assets/TodoHeadIcon.tsx";
+import {c} from "vite/dist/node/types.d-aGj9QkWt";
 
 function App() {
-interface TodoItem  {
-    id: number,
-    taskName : string,
-    isDone : boolean
-}
-const [lastItem, setLastItem] = useState(0)
-const [list, setList] = useState<TodoItem[]>([])
-const [inputValue, setInputValue] = useState<string>('')
-const handleChange:ChangeEventHandler<HTMLInputElement> =(e) =>{
-    setInputValue(e.target.value)
-}
-const handleSubmit: MouseEventHandler<HTMLButtonElement> =(e) =>{
-    e.preventDefault()
-    setList([...list,  {
-        id: lastItem,
-        taskName: inputValue,
-        isDone: false
-    }])
-    setLastItem(prev => prev+1)
-    setInputValue('')
-}
-const handleDelete = (item: number) => {
-    // @ts-ignore
-    const filteredTodos = list.filter((todo) =>  todo.id !== item)
-    setList([...filteredTodos])
-}
-  return (
-      <>
-          <div className="todo-head">
-            <TodoHeadIcon/>
-            <div className="fs-32 bold">To-Do List</div>
-          </div>
-          <div className="input-field">
-              <input value={inputValue} onChange={handleChange} className="input" type="text" placeholder={"Add your task"}/>
-              <button onClick={handleSubmit} className="button">ADD +</button>
-          </div>
-          <div className="todos">
-              {list.map((todo) => {
-                  return (
-                      <div key={todo.id}>
-                          <Todo  onDelete = {() => handleDelete(todo.id)} todo={todo}/>
-                      </div>
-                  )
-              })}
-          </div>
-      </>
-  )
+    interface TodoItem  {
+        id: number,
+        taskName : string,
+        isDone : boolean
+    }
+    const [lastItemId, setLastItemId] = useState(0)
+    const [list, setList] = useState<TodoItem[]>([])
+    const [inputValue, setInputValue] = useState<string>('')
+    const handleChange:ChangeEventHandler<HTMLInputElement> =(e) =>{
+        setInputValue(e.target.value)
+    }
+    const handleChangeItem = (newTodo) => {
+        const changedList = list.map(todo => {
+            if(todo.id === newTodo.id) {
+                return newTodo
+            }
+            return todo
+        })
+        setList([...changedList])
+    }
+    const handleSubmit: MouseEventHandler<HTMLButtonElement> =(e) =>{
+        e.preventDefault()
+        setLastItemId(prev => prev+1)
+        setList([...list,  {
+            id: lastItemId,
+            taskName: inputValue,
+            isDone: false
+        }])
+        setInputValue('')
+    }
+    const handleDelete = (id: number) => {
+        const filteredTodos = list.filter((todo) =>  todo.id !== id)
+        setList([...filteredTodos])
+    }
+    return (
+        <>
+            <div className="todo-head">
+                <TodoHeadIcon/>
+                <div className="fs-32 bold">To-Do List</div>
+            </div>
+            <div className="input-field">
+                <input value={inputValue} onChange={handleChange} className="input" type="text" placeholder={"Add your task"}/>
+                <button onClick={handleSubmit} className="button">ADD +</button>
+            </div>
+            <div className="todos">
+                {list.map((todo) => {
+                    return (
+                        <div id={todo.id} key={todo.id}>
+                            <Todo onChange={handleChangeItem}  onDelete = {() => handleDelete(todo.id)} todo={todo}/>
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    )
 }
 
 export default App
--- src/Todo/Todo.tsx
+++ src/Todo/Todo.tsx
@@ -3,23 +3,23 @@ import {Checkbox} from "@mui/material";
 import {ChangeEventHandler, MouseEventHandler} from "react";
 import {TrashIcon} from "../assets/TrashIcon.tsx";
 
-export const Todo = ({todo, onDelete} : any) => {
+export const Todo = ({todo, onDelete, onChange} : any) => {
     const handleDelete:MouseEventHandler<HTMLButtonElement> = (e) => {
         e.stopPropagation()
         onDelete()
     }
     const handleChangeDone: ChangeEventHandler<HTMLInputElement> = (e) => {
-        console.log(todo.isDone)
-        todo.isDone = e.target.checked
+        const newTodo = {...todo, isDone:e.target.checked}
+        onChange(newTodo)
     }
     const handleChangeTaskName: ChangeEventHandler<HTMLInputElement> = (e) => {
-        todo.taskName = e.target.value
-
+        const newTodo = {...todo, taskName:e.target.value}
+        onChange(newTodo)
     }
     return (
         <div className="item">
             <Checkbox id={todo.id} checked={todo.isDone}  className="checkbox" onChange={handleChangeDone} inputProps={{'aria-label': 'Checkbox demo' }} color="success" />
-            <input onChange={handleChangeTaskName} className="todo-name fs-24 bold" value={todo.taskName}/>
+            <input onChange={handleChangeTaskName} className={`todo-name ${todo.isDone ? "active" : ""} fs-24 bold`} value={todo.taskName}/>
             <button onClick={handleDelete} className="delete"><TrashIcon/></button>
         </div>
     )
